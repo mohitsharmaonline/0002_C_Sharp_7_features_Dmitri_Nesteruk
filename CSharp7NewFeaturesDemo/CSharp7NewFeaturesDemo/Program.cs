4c9796b4092887394e6655285603b019ad8c8e9f
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-12-15 15:18:07 +0530

    Feature 1: What if the parsing fails ?

--- CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
+++ CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
@@ -25,6 +25,18 @@ namespace CSharp7NewFeaturesDemo
              *  2/2/2016 12:00:00 AM
              *  dt outside block: 2/2/2016 12:00:00 AM
             ********************************************************/
+
+            // What if the parsing fails and we use out variable?
+            int.TryParse("abc", out var i);
+            Console.WriteLine($"value of i after parsing fails: {i}");
+
+            /*********************************************************
+             *  Output
+             *  2/2/2016 12:00:00 AM
+             *  dt outside block: 2/2/2016 12:00:00 AM
+             *  value of i after parsing fails: 0
+            *********************************************************/
+            // so in the above case output will be the default value of variable data type.
         }
     }
 }
