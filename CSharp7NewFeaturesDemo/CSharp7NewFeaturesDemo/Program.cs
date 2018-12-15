0e05b0eb444065b03ec4c14cd6f633b4f78bec49
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-12-15 18:01:09 +0530

    Feature 4: Local Functions. introduction.

--- CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
+++ CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
@@ -10,29 +10,8 @@ namespace CSharp7NewFeaturesDemo
     {
         static void Main(string[] args)
         {
-            // now you can make sure that even one of your type supports deconstruction
-            // just like tuples.
-            var myPoint = new Point { X = 5, Y = 3 };
-            // now you want to deconstruct your point:
-            // if you don't care about the second return value.
-            var (x, _) = myPoint;
-            Console.WriteLine($"myPoint X: {x}");
-
-            /*******************************************************************
-             * Output:
-             * myPoint X: 5
-             *****************************************************************/
-        }
-
-        public class Point
-        {
-            public int X, Y;
-
-            public void Deconstruct(out int x, out int y)
-            {
-                x = X;
-                y = Y;
-            }
+            // Local Functions
+            // this feature already existed in C# but under a disguise.
         }
     }
 }
