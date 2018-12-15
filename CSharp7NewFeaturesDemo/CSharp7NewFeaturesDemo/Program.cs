683640f951b7517aeebf1b3e7213f427bf9a0b0c
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-12-15 18:38:29 +0530

    Feature 4: Local Function: Using Local Functions as lambda, capturing local states.

--- CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
+++ CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
@@ -12,12 +12,20 @@ namespace CSharp7NewFeaturesDemo
         public static Tuple<double, double>
             SolveQuadratic(double a, double b, double c)
         {
-            double CalculateDiscriminant(double aa, double bb, double cc)
+            //double CalculateDiscriminant(double aa, double bb, double cc)
+            //{
+            //    return bb * bb - 4 * aa * cc;
+            //}
+
+            // In addition, we can have thid function also capture a, b and c as if
+            // hey were a lambda. because lambda functions can capture whatever is
+            // available within their scope.
+            double CalculateDiscriminant()
             {
-                return bb * bb - 4 * aa * cc;
+                return b * b - 4 * a * c;
             }
 
-            var disc = CalculateDiscriminant(a, b, c);
+            var disc = CalculateDiscriminant();
             var rootDisc = Math.Sqrt(disc);
             return Tuple.Create(
                 (-b-rootDisc) / (2*a),
