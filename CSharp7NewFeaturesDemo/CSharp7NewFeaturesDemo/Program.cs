bd22ac04c1b4caf215600cec501720cee2fa9711
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-12-15 17:07:51 +0530

    Feature 3: Tuples : stry continues 1

--- CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
+++ CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
@@ -30,6 +30,34 @@ namespace CSharp7NewFeaturesDemo
             var sp2 = NewSumAndProduct(2, 5);
             Console.WriteLine($"sum = {sp2.sum}, product= {sp2.product}");
             Console.WriteLine(sp2.GetType());
+
+            // you can also store parts of the tuple in their own saparate variables.
+            // we are explicitely declarring types and name of variables here
+            (double ss, double pp) = NewSumAndProduct(2, 3);
+
+            // Second approach lets you infer the types.
+            var (sum1, product1) = NewSumAndProduct(1, 2);
+
+            // you can also declare variables beforehand
+            double s, p;
+            (s, p) = NewSumAndProduct(1, 2);
+
+            // using tuples right inside main code.
+            var me = (name: "mohit", age: 230);
+            Console.WriteLine(me);
+            Console.WriteLine(me.GetType());
+
+            /**************************************************************************
+             * Output
+             * sum = 7, product = 10
+             * sum = 7, product= 10
+             * System.ValueTuple`2[System.Double,System.Double]
+             * (mohit, 230)
+             * System.ValueTuple`2[System.String,System.Int32]
+             * **********************************************************************/
+
+            Console.WriteLine($"My name is {me.name}, I am {me.age} years old.");
+
         }
     }
 }
