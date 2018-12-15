4464291113a8ae0a775fb48da31704b24387b267
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-12-15 16:21:53 +0530

    Feature 3: Tuples : Introduction to new changes in tuples.

--- CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
+++ CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
@@ -13,6 +13,11 @@ namespace CSharp7NewFeaturesDemo
             return Tuple.Create(a + b, a * b);
         }
 
+        static (double sum, double product) NewSumAndProduct(double a, double b)
+        {
+            return (a + b, a * b);
+        }
+
         static void Main(string[] args)
         {
             // Feature 3: Tuples
@@ -21,6 +26,10 @@ namespace CSharp7NewFeaturesDemo
 
             // here is exacly where the fun ends before C# 7
             Console.WriteLine($"sum = {sp.Item1}, product = {sp.Item2}");
+
+            var sp2 = NewSumAndProduct(2, 5);
+            Console.WriteLine($"sum = {sp2.sum}, product= {sp2.product}");
+            Console.WriteLine(sp2.GetType());
         }
     }
 }
