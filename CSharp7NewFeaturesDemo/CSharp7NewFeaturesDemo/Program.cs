0c20865b473ef1579a985e97f7a983862944e116
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-12-15 16:02:46 +0530

    Feature 3: Tuples : Demo of problem before C# 7 with Tuples.

--- CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
+++ CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
@@ -6,67 +6,21 @@ using System.Threading.Tasks;
 
 namespace CSharp7NewFeaturesDemo
 {
-    public class Shape
-    {
-
-    }
-
-    public class Rectangle : Shape
-    {
-        public int Width, Height;
-    }
-
-    public class Circle : Shape
-    {
-
-    }
-
     class Program
-    {       
-
-        public void DisplayShape(Shape shape)
+    {
+        static Tuple<double, double> SumAndProduct(double a, double b)
         {
-            if(shape is Rectangle r)
-            {
-                // Use r
-            }
-
-            // variable r is available here too.
-
-            if (!(shape is Circle notCircle))
-            {
-                // use notCircle here
-            }
-
-            // notCircle is available here too.
-
-            switch(shape)
-            {
-                case Circle c:
-                    // use c
-                    break;
-                case Rectangle sq when (sq.Height == sq.Width):
-                    // use sq
-                    break;
-            }
-
-            // unfortunately we haven't got here any kind of switching on tuples
-            var z = (12, 23);
-            switch(z)
-            {
-                // We still can't do it!
-                case (0, 0):
-                    // we have origin.
-                    break;
-            }
+            return Tuple.Create(a + b, a * b);
         }
 
         static void Main(string[] args)
         {
-            // Feature 2: Pattern Matching
-            // Unfortunately this feature is coming very slowly.
-            // So in C#7 we never really get saveral features that could have been there
-            // with pattern matching.            
+            // Feature 3: Tuples
+            // Tuples have already existed in C#
+            var sp = SumAndProduct(2, 5);
+
+            // here is exacly where the fun ends before C# 7
+            Console.WriteLine($"sum = {sp.Item1}, product = {sp.Item2}");
         }
     }
 }
