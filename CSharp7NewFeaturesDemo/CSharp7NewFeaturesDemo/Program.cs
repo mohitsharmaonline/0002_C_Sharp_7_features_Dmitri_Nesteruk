4f4ce23a400d63ca08963819a3196bcfa273c0aa
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-12-15 15:12:10 +0530

    Feature 1: Out Variable Initial display of out variables.

--- CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
+++ CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
@@ -11,12 +11,20 @@ namespace CSharp7NewFeaturesDemo
         static void Main(string[] args)
         {
             // Feature 1: Out Variables
-            // Old way before c# 7: variable declaratio is outside.
-            DateTime dt;
-            if(DateTime.TryParse("01/01/2017", out dt))
+            if (DateTime.TryParse("02/02/2016", out DateTime dt))
             {
                 Console.WriteLine(dt);
             }
+
+            // Interestingly the variable dt is now available outside the
+            // block where it is defined too. For example:
+            Console.WriteLine($"dt outside block: {dt}");
+
+            /*********************************************************
+             *  Output
+             *  2/2/2016 12:00:00 AM
+             *  dt outside block: 2/2/2016 12:00:00 AM
+            ********************************************************/
         }
     }
 }
