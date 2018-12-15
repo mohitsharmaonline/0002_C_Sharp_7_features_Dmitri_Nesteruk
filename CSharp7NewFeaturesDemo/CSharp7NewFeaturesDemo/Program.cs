e0fbeb4bf788c8a1b98607310d8aa2de1631e97c
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-12-15 19:35:36 +0530

    Feature 5: Ref Returns and Locals : More plays with it. Part 1

--- CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
+++ CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
@@ -21,7 +21,7 @@ namespace CSharp7NewFeaturesDemo
 
             // there are couple of rules, you can not rebind the variables
             // you can not go ahead with same variable to reference something else.
-            refToSecond = ref numbers[0];
+            //refToSecond = ref numbers[0];
 
             /*******************************************************************
              * ERROR
@@ -37,6 +37,35 @@ namespace CSharp7NewFeaturesDemo
              * Output
              * 1,123,3
              * **********************************************************/
+
+            // The reference is going to persist even if the array is no longer available.
+            Array.Resize(ref numbers, 1);
+            // now the array is a of lenght 1 only, still we got our reference.
+            Console.WriteLine($"Second = {refToSecond}, array size is {numbers.Length}");
+
+            /***************************************************************
+             * output
+             * 1,123,3
+             * Second = 123, array size is 1
+             * ************************************************************/
+            // we can still manipulate it like before
+            refToSecond = 321;
+            Console.WriteLine($"Second = {refToSecond}, array size is {numbers.Length}");
+
+            /*******************************************************************************
+             * 1,123,3
+             * Second = 123, array size is 1
+             * Second = 321, array size is 1
+             * *****************************************************************************/
+            // Below line will throw exception as array bounds are violated.
+            //numbers.SetValue(321, 1);
+
+            // Can't expect same from list.
+            //var numberList = new List<int> { 1, 2, 3 };
+            //ref int second = ref numberList[1];
+            // Error will come:
+            // Severity	Code	Description	Project	File	Line	Suppression State
+            // Error CS0206  A property or indexer may not be passed as an out or ref parameter CSharp7NewFeaturesDemo  C: \Users\mohit\Documents\0002_C_Sharp_7_features_Dmitri_Nesteruk\CSharp7NewFeaturesDemo\CSharp7NewFeaturesDemo\Program.cs   64  Active
 
         }
     }
