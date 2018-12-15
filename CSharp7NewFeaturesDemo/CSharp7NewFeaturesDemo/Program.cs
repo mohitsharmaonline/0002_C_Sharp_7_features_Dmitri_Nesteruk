9bcd12bdfc1c680dc9f715cf08c87e5e9e5b3049
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-12-15 18:15:54 +0530

    Feature 4: Local Functions: Rather convoluted way of putting functions within a function using Func's before C# 7.

--- CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
+++ CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
@@ -6,12 +6,43 @@ using System.Threading.Tasks;
 
 namespace CSharp7NewFeaturesDemo
 {
+
+    public class EquationSolver
+    {
+        public static Tuple<double, double>
+            SolveQuadratic(double a, double b, double c)
+        {
+            // To solve a quadratic equation, first
+            // we need to figure out a discriminate, which is 
+            // b*b-4*a*c
+
+            // one place where we can calculate disciminant is inside a Func
+            var CalculateDiscriminant = new Func<double, double, double, double>(
+                (aa, bb, cc) => bb * bb - 4 * aa * cc);
+            // this is lengthy, but it works!
+
+            var disc = CalculateDiscriminant(a, b, c);
+            var rootDisc = Math.Sqrt(disc);
+            return Tuple.Create(
+                (-b-rootDisc) / (2*a),
+                (-b+rootDisc) / (2*a)
+                );
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             // Local Functions
             // this feature already existed in C# but under a disguise.
+            var result = EquationSolver.SolveQuadratic(1, 10, 16);
+            Console.WriteLine(result);
+
+            /****************************************************
+             * Output:
+             * (-8, -2)
+             * **************************************************/
         }
     }
 }
