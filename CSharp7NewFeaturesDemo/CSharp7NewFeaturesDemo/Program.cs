9f2e0aa3f0fe62654f8e79ed9f0912d328c6d0bb
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-12-16 14:01:39 +0530

    Feature 8: generalized async return types: Demo of functionalities available with C# 7.

--- CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
+++ CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -6,7 +7,26 @@ namespace CSharp7NewFeaturesDemo
 {
     class Program
     {
+        // C# 7 allows custom return types.
         public static async Task<long> GetDirectorySize(string dir)
+        {
+            if (!Directory.EnumerateFileSystemEntries(dir).Any())
+            {
+                return 0;
+                // now unfortunately this return of 0, would also actually imply creation of a 
+                // task, even if you do not really need it.
+            }
+            return await Task.Run(() =>
+                Directory.GetFiles(dir, "*.*").Sum(file => new FileInfo(file).Length)
+                );
+
+            // This is how you will implement it in C#6 and this is fine just that
+            // even an empty directory implies creation of a Task.
+        }
+
+        // For using ValueTasks, you need to have Nuget package
+        // System.Threading.Tasks.Extensions
+        public static async ValueTask<long> GetDirSize(string dir)
         {
             if (!Directory.EnumerateFileSystemEntries(dir).Any())
             {
@@ -28,6 +48,12 @@ namespace CSharp7NewFeaturesDemo
             // Another constraint with Async methods prior to C# 7 was that async methods
             // were required to return either void or Task for a Task of T.
             // now what C# 7 allows us to do is to return
+            Console.WriteLine(GetDirSize(@"C:\Users\mohit\Documents\0001_Pluralsight_WPF_XAML_Fundamentals").Result);
+
+            /*******************************************************************************************
+             * Output
+             * 6148
+             * ****************************************************************************************/
         }
     }
 }
