59c05d8a4082af21e9e181edf450878f49faea00
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-12-16 12:40:49 +0530

    Feature 6: Expression bodied members. Demo of all new features.

--- CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
+++ CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
@@ -6,6 +6,24 @@ using System.Threading.Tasks;
 
 namespace CSharp7NewFeaturesDemo
 {
+    public class Person
+    {
+        private int _id;
+        private static readonly Dictionary<int, string> _names = new Dictionary<int, string>();
+
+        //new feature of c# 7, lambda expressions for constructor can be used only in c#7 onwards.
+        public Person(int id, string name) => _names.Add(id, name);
+        ~Person() => _names.Remove(_id);
+
+        // we can use expression bodies even with gettters and setters also fro c# 7 onwards.
+        public string Name
+        {
+            get => _names[_id];
+            set => _names[_id] = value;
+        }
+
+        // These new features of C# were contributed by community, rather than made by Microsoft.
+    }
     class Program
     {
         static void Main(string[] args)
