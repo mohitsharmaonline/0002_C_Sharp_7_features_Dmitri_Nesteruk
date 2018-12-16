53549b930da0915ce4fc0c01c07ee933a1337810
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-12-16 13:05:45 +0530

    Feature 7: throw Expressions: Demo with all new features.

--- CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
+++ CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
@@ -12,12 +12,13 @@ namespace CSharp7NewFeaturesDemo
 
         public Program(string name)
         {
-            if(name == null)
-            {
-                throw new ArgumentNullException(paramName: nameof(name));
-            }
+            // New syntax allowed in C# 7.
+            Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+        }
 
-            Name = name;
+        int GetValue(int n)
+        {
+            return n > 0 ? n + 1 : throw new Exception();
         }
 
         static void Main(string[] args)
@@ -28,6 +29,33 @@ namespace CSharp7NewFeaturesDemo
             // with C# 7 , we are kind of breaking that, because now a ternary expression 
             // and a conditional null coalesing expression can actually contain a throw 
             // staement as their part.
+
+            int val = -2;
+            try
+            {
+                var d = new Program("");
+                val = d.GetValue(-1);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);                
+            }
+            finally
+            {
+                Console.WriteLine(val);
+            }
+
+            /*******************************************************************************
+             * System.Exception: Exception of type 'System.Exception' was thrown.
+             * at CSharp7NewFeaturesDemo.Program.GetValue(Int32 n) in C:\Users\mohit\Documen
+             * ts\0002_C_Sharp_7_features_Dmitri_Nesteruk\CSharp7NewFeaturesDemo\CSharp7NewFeat
+             * uresDemo\Program.cs:line 21
+             * at CSharp7NewFeaturesDemo.Program.Main(String[] args) in C:\Users\mohit\Docum
+             * ents\0002_C_Sharp_7_features_Dmitri_Nesteruk\CSharp7NewFeaturesDemo\CSharp7NewFe
+             * aturesDemo\Program.cs:line 37
+             * -2
+             * ****************************************************************************/
         }
     }
 }
