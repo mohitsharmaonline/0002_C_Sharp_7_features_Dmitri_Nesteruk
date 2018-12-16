a005a1bdc20c7272bfc319b90d4676df8c8b4713
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-12-16 12:49:15 +0530

    Feature 7: throw Expressions: Introduction.

--- CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
+++ CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
@@ -5,33 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CSharp7NewFeaturesDemo
-{
-    public class Person
-    {
-        private int _id;
-        private static readonly Dictionary<int, string> _names = new Dictionary<int, string>();
-
-        //new feature of c# 7, lambda expressions for constructor can be used only in c#7 onwards.
-        public Person(int id, string name) => _names.Add(id, name);
-        ~Person() => _names.Remove(_id);
-
-        // we can use expression bodies even with gettters and setters also fro c# 7 onwards.
-        public string Name
-        {
-            get => _names[_id];
-            set => _names[_id] = value;
-        }
-
-        // These new features of C# were contributed by community, rather than made by Microsoft.
-    }
+{    
     class Program
     {
         static void Main(string[] args)
-        {  
-            // Feature 6: Expression bodied members.
-            // C# already have this concept of Expression bodies which lets you shorten the
-            // methods, but now this idea has been extended to other things like constructors and getters
-            // and setters and so on.
+        {
+            // Feature 7: throw Expressions.
+            // if you think about ternary expressions or null coalesing expressions 
+            // you will really think about both of them to be identical. 
+            // with C# 7 , we are kind of breaking that, because now a ternary expression 
+            // and a conditional null coalesing expression can actually contain a throw 
+            // staement as their part.
         }
     }
 }
