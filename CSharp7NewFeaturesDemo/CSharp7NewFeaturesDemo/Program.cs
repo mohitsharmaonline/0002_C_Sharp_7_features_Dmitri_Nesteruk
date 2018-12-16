fd4c5015e562d9d2068d9b2a4e8376f95e149b5b
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-12-16 13:24:07 +0530

    Feature 8: generalized async return types: Introduction with demo of how things used to work before C#7 and the problem with it.

--- CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
+++ CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
@@ -1,61 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace CSharp7NewFeaturesDemo
-{    
+{
     class Program
     {
-        public string Name { get; set; }
-
-        public Program(string name)
+        public static async Task<long> GetDirectorySize(string dir)
         {
-            // New syntax allowed in C# 7.
-            Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
-        }
+            if (!Directory.EnumerateFileSystemEntries(dir).Any())
+            {
+                return 0;
+                // now unfortunately this return of 0, would also actually imply creation of a 
+                // task, even if you do not really need it.
+            }
+            return await Task.Run(() =>
+                Directory.GetFiles(dir, "*.*").Sum(file => new FileInfo(file).Length)
+                );
 
-        int GetValue(int n)
-        {
-            return n > 0 ? n + 1 : throw new Exception();
+            // This is how you will implement it in C#6 and this is fine just that
+            // even an empty directory implies creation of a Task.
         }
 
         static void Main(string[] args)
         {
-            // Feature 7: throw Expressions.
-            // if you think about ternary expressions or null coalesing expressions 
-            // you will really think about both of them to be identical. 
-            // with C# 7 , we are kind of breaking that, because now a ternary expression 
-            // and a conditional null coalesing expression can actually contain a throw 
-            // staement as their part.
-
-            int val = -2;
-            try
-            {
-                var d = new Program("");
-                val = d.GetValue(-1);
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);                
-            }
-            finally
-            {
-                Console.WriteLine(val);
-            }
-
-            /*******************************************************************************
-             * System.Exception: Exception of type 'System.Exception' was thrown.
-             * at CSharp7NewFeaturesDemo.Program.GetValue(Int32 n) in C:\Users\mohit\Documen
-             * ts\0002_C_Sharp_7_features_Dmitri_Nesteruk\CSharp7NewFeaturesDemo\CSharp7NewFeat
-             * uresDemo\Program.cs:line 21
-             * at CSharp7NewFeaturesDemo.Program.Main(String[] args) in C:\Users\mohit\Docum
-             * ents\0002_C_Sharp_7_features_Dmitri_Nesteruk\CSharp7NewFeaturesDemo\CSharp7NewFe
-             * aturesDemo\Program.cs:line 37
-             * -2
-             * ****************************************************************************/
+            // Feature 8: generalized async return types
+            // Another constraint with Async methods prior to C# 7 was that async methods
+            // were required to return either void or Task for a Task of T.
+            // now what C# 7 allows us to do is to return
         }
     }
 }
