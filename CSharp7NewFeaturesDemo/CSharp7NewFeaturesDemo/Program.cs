1ee7e2c81aa3ef7a75ba8c8818a036a042743d5e
Mohit Sharma <mohitsharmaonline@gmail.com>
2018-12-16 14:22:03 +0530

    Feature 9: Literal Improvements : all the new changes.

--- CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
+++ CSharp7NewFeaturesDemo/CSharp7NewFeaturesDemo/Program.cs
@@ -6,54 +6,24 @@ using System.Threading.Tasks;
 namespace CSharp7NewFeaturesDemo
 {
     class Program
-    {
-        // C# 7 allows custom return types.
-        public static async Task<long> GetDirectorySize(string dir)
-        {
-            if (!Directory.EnumerateFileSystemEntries(dir).Any())
-            {
-                return 0;
-                // now unfortunately this return of 0, would also actually imply creation of a 
-                // task, even if you do not really need it.
-            }
-            return await Task.Run(() =>
-                Directory.GetFiles(dir, "*.*").Sum(file => new FileInfo(file).Length)
-                );
-
-            // This is how you will implement it in C#6 and this is fine just that
-            // even an empty directory implies creation of a Task.
-        }
-
-        // For using ValueTasks, you need to have Nuget package
-        // System.Threading.Tasks.Extensions
-        public static async ValueTask<long> GetDirSize(string dir)
-        {
-            if (!Directory.EnumerateFileSystemEntries(dir).Any())
-            {
-                return 0;
-                // now unfortunately this return of 0, would also actually imply creation of a 
-                // task, even if you do not really need it.
-            }
-            return await Task.Run(() =>
-                Directory.GetFiles(dir, "*.*").Sum(file => new FileInfo(file).Length)
-                );
-
-            // This is how you will implement it in C#6 and this is fine just that
-            // even an empty directory implies creation of a Task.
-        }
+    {       
 
         static void Main(string[] args)
         {
-            // Feature 8: generalized async return types
-            // Another constraint with Async methods prior to C# 7 was that async methods
-            // were required to return either void or Task for a Task of T.
-            // now what C# 7 allows us to do is to return
-            Console.WriteLine(GetDirSize(@"C:\Users\mohit\Documents\0001_Pluralsight_WPF_XAML_Fundamentals").Result);
+            // Feature 9: Literal Improvements
 
-            /*******************************************************************************************
-             * Output
-             * 6148
-             * ****************************************************************************************/
+            // you can add underscores to the variables
+            int a = 1_23_321; // Underscores should actually be used to delimit millions, thousands etc.
+            // However they can be inserted anywhere.
+            // you can add as many underscores as you like, all of them will be ignored by the compiler.
+            int b = 123_321_________123;
+
+            // valid on hexadecimals
+            long h = 0xAB_BC_D123EF;
+
+            // valid on binary
+            var bin = 0b1110_0111_0111;
+
         }
     }
 }
